09d67755e6e5f5f22bc3f105bbbe1a2bcd74ea7a
JESSICA <95001228+desarrolloJess@users.noreply.github.com>
2024-03-01 18:13:06 -0600

    Se termina proyecto 01/03/2024

--- main.py
+++ main.py
@@ -38,6 +38,51 @@ def ABC_Completo():
     empleado = Empleados.query.all()
     return render_template("ABC_Completo.html",empleado=empleado)
 
+@app.route("/eliminar",methods=["GET","POST"])
+def eliminar():
+    empleados_form=forms.EmpleadosForm(request.form)
+    if request.method == 'GET':
+        id=request.args.get('id')
+        empleados1= db.session.query(Empleados).filter(Empleados.id==id).first()
+        empleados_form.id.data = request.args.get('id')
+        empleados_form.nombre.data=empleados1.nombre
+        empleados_form.correo.data=empleados1.correo
+        empleados_form.direccion.data=empleados1.direccion
+        empleados_form.telefono.data=empleados1.telefono
+        empleados_form.sueldo.data=empleados1.sueldo
+    if request.method == 'POST':
+        id=empleados_form.id.data
+        emp = Empleados.query.get(id)
+        db.session.delete(emp)
+        db.session.commit()
+        return redirect('ABC_Completo')
+    return render_template("eliminar.html",form=empleados_form)
+
+@app.route("/modificar",methods=["GET","POST"])
+def modificar():
+    empleados_form=forms.EmpleadosForm(request.form)
+    if request.method == 'GET':
+        id=request.args.get('id')
+        empleado1= db.session.query(Empleados).filter(Empleados.id==id).first()
+        empleados_form.id.data = request.args.get('id')
+        empleados_form.nombre.data=empleado1.nombre
+        empleados_form.correo.data=empleado1.correo
+        empleados_form.telefono.data=empleado1.telefono
+        empleados_form.direccion.data=empleado1.direccion
+        empleados_form.sueldo.data=empleado1.sueldo
+    if request.method == 'POST':
+        id=empleados_form.id.data
+        emp1= db.session.query(Empleados).filter(Empleados.id==id).first()
+        emp1.nombre=empleados_form.nombre.data
+        emp1.correo=empleados_form.correo.data
+        emp1.telefono=empleados_form.telefono.data
+        emp1.direccion=empleados_form.direccion.data
+        emp1.sueldo=empleados_form.sueldo.data
+        db.session.add(emp1)
+        db.session.commit()
+        return redirect('ABC_Completo')
+    return render_template("modificar.html",form=empleados_form)
+
 
 if __name__ == "__main__":
     csrf.init_app(app)
